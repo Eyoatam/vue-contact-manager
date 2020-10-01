ad4374230674a286c4c0841bec1e6f6563a20202
Eyoatam Tamirat <eyoatamtamirat7@gmail.com>
2020-10-01 12:36:22 +0300

    remove unwanted code

--- src/App.vue
+++ src/App.vue
@@ -16,9 +16,6 @@ export default {
 };
 </script>
 
-{ primary: '#1976D2', secondary: '#424242', accent: '#82B1FF', error: '#FF5252',
-info: '#2196F3', success: '#4CAF50', warning: '#FFC107', }
-
 <style>
 @import url("https://fonts.googleapis.com/css2?family=Nunito:wght@400;700&display=swap");
 h2 {
